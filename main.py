9fbed0c578060edd014d7d40d3ba24afe82e4dca
bobur <valiyev7115@gmail.com>
2025-02-11 08:56:51 +0500

    Palimarfizim

--- main.py
+++ main.py
@@ -23,4 +23,10 @@
 
 
 
-lst = [1, 2, 5, 9, 20, 70, -8 ]
+# lst = [1, 2, 3, 4, 5, 6, 7, 8, 80, -2]
+# largest = 0
+#
+# for i in range(len(lst)):
+#     if lst[i] > lst[i - 1]:
+#         largest = lst[i]
+# print(largest)
